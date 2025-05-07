960f7ca514cd59920b5713b096939f4ad4fe6097
MohammadG4 <99698684+MohammadG4@users.noreply.github.com>
2025-05-07 19:09:09 +0300

    20231109

--- task.java
+++ task.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("20230362");
         System.out.println("20230446");
+        System.out.println("20231109");
     }
 }
